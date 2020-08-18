6bcfce28b10477c77532d243144682c3a725244d
Karemoi <kareema.java@gmail.com>
2020-08-18 08:58:28 -0500

    Add files via upload

---
+++ src/redux/store.js
@@ -0,0 +1,23 @@
+import { createStore, applyMiddleware, compose } from "redux";
+import { createBrowserHistory } from "history";
+import { routerMiddleware } from "connected-react-router";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers";
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export const history = createBrowserHistory();
+
+const middleware =
+  process.env.NODE_ENV !== "production"
+    ? [
+        require("redux-immutable-state-invariant").default(),
+        thunk,
+        routerMiddleware(history)
+      ]
+    : [thunk, routerMiddleware(history)];
+
+export default createStore(
+  rootReducer(history),
+  composeEnhancer(applyMiddleware(...middleware))
+);
